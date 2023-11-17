8a283eaed7a6f6795be32b2ef1ebc79ca513341c
cryptobytehub <139621652+cryptobytehub@users.noreply.github.com>
2023-11-17 15:52:06 +0100

    Update arch.c

--- lk/arch/x86/arch.c
+++ lk/arch/x86/arch.c
@@ -20,7 +20,6 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include <debug.h>
 #include <arch.h>
 #include <arch/ops.h>
 #include <arch/x86.h>
